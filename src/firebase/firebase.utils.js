f8c4e3fa4e42657fbe201e61323448b25855e142
thomsenc <christian.thomsen@aviture.us.com>
2020-11-05 08:33:39 -0600

    Updates

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,42 +3,77 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyC-4324bvofSOZ4IlmVQdU7cfhuLRQFHWI",
-  authDomain: "crwn-db-63b49.firebaseapp.com",
-  databaseURL: "https://crwn-db-63b49.firebaseio.com",
-  projectId: "crwn-db-63b49",
-  storageBucket: "crwn-db-63b49.appspot.com",
-  messagingSenderId: "657552512027",
-  appId: "1:657552512027:web:f0b652277cdf3c526c14be",
-  measurementId: "G-HY7BNN4K9K",
-};
-
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
-
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
-
-  const snapShot = await userRef.get();
-  if (!snapShot.exists) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
-
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        ...additionalData,
-      });
-    } catch (error) {
-      console.log("error creating user", error.message);
-    }
-  }
-
-  return userRef;
+    apiKey: "AIzaSyC-4324bvofSOZ4IlmVQdU7cfhuLRQFHWI",
+    authDomain: "crwn-db-63b49.firebaseapp.com",
+    databaseURL: "https://crwn-db-63b49.firebaseio.com",
+    projectId: "crwn-db-63b49",
+    storageBucket: "crwn-db-63b49.appspot.com",
+    messagingSenderId: "657552512027",
+    appId: "1:657552512027:web:f0b652277cdf3c526c14be",
+    measurementId: "G-HY7BNN4K9K",
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData,
+            });
+        } catch (error) {
+            console.log("error creating user", error.message);
+        }
+    }
+
+    return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+) => {
+    const collectionRef = firestore.collection(collectionKey);
+    console.log(collectionRef);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach((obj) => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map((doc) => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items,
+        };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
